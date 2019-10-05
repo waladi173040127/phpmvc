b8e39b0e30ce1cd0fd8b5ded9f97e13cd218cc11
waladi173040127 <waladi.173040127@mail.unpas.ac.id>
2019-10-05 23:35:41 +0700

    menyelasikan video

---
+++ app/controllers/About.php
@@ -0,0 +1,9 @@
+<?php 
+class About{
+	public function index(){
+		echo "Halo, nama saya waladi, saya adalah seorang mahasiswa";
+	}
+	public function page(){
+		echo "About/page";
+	}
+}
--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,6 +1,6 @@
 <?php 
 
-class Controlles {
+class Controller {
 
 
 
