86f9b855d9fbb9ce1ec04787587176f9a9fe5ad9
waladi173040127 <waladi.173040127@mail.unpas.ac.id>
2019-10-06 11:10:27 +0700

    menambahkan fungsi hapus data #11

--- app/models/mahasiswa_model.php
+++ app/models/mahasiswa_model.php
@@ -39,4 +39,13 @@ class Mahasiswa_model{
 		
 	}
 
+	public function hapusDataMahasiswa($id){
+		$query = "DELETE FROM mahasiswa WHERE id = :id";
+		$this->db->query($query);
+		$this->db->bind('id', $id);
+
+		$this->db->execute();
+		return $this->db->rowCount();
+	}
+
 }
