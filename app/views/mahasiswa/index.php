706b944c640fd0aa1702537296e71bb771a83a88
waladi173040127 <waladi.173040127@mail.unpas.ac.id>
2019-10-06 10:03:53 +0700

    menerapkan database wrapper

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,17 +1,18 @@
 <div class="container mt-5">
 	<div class="row">
 		<div class="col-6">
-			<h3>Daftar mahasiswa</h3>
-
-		
-				<?php foreach ($data['mhs'] as $mhs) :?>
-					<ul>
-						<li><?= $mhs['nama']; ?></li>
-						<li><?= $mhs['nrp']; ?></li>
-						<li><?= $mhs['email']; ?></li>
-						<li><?= $mhs['jurusan']; ?></li>
-					</ul>
-				<?php endforeach;?>
+			 Tambah Data Mahasiswa
+			</button>
+			<br><br>
+			<h3>Daftar Mahasiswa</h3>
+			<ul class="list-group">
+				<?php foreach( $data['mhs'] as $mhs )  : ?>
+			  	<li class="list-group-item d-flex justify-content-between align-items-center">
+			  		<?= $mhs['nama']; ?>
+			  	    <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id'];?>" class="badge badge-primary ">detail </a>
+			  	</li>
+			  	<?php endforeach; ?>
+			</ul>
 			
 		</div>
 	</div>
