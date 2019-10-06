0df6f5babcfeb97701e84032d666cbd22c2f8454
waladi173040127 <waladi.173040127@mail.unpas.ac.id>
2019-10-06 10:38:47 +0700

    menambahkan fungsi insert data #9

--- app/controllers/mahasiswa.php
+++ app/controllers/mahasiswa.php
@@ -17,5 +17,18 @@ class Mahasiswa extends Controller{
 		$this->view('mahasiswa/detail', $data);
 		$this->view('templates/footer');
 	}
+
+	public function tambah()
+	{
+		if( $this->model('Mahasiswa_model')->tambahDataMahasiswa($_POST) > 0 ){
+			Flasher::setFlash(' berhasil ', 'ditambahkan', 'success ');
+			header('Location: ' . BASEURL . '/mahasiswa');
+			exit;
+		}else{
+			Flasher::setFlash('gagal', 'ditambahkan', 'danger');
+			header('Location: ' . BASEURL . '/mahasiswa');
+			exit;
+		}
+	}
 	
 }
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,7 +1,9 @@
-<div class="container mt-5">
+<div class="container mt-3">
 	<div class="row">
 		<div class="col-6">
-			 Tambah Data Mahasiswa
+			 <!-- Button trigger modal -->
+			<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+			  Tambah Data Mahasiswa
 			</button>
 			<br><br>
 			<h3>Daftar Mahasiswa</h3>
@@ -20,3 +22,50 @@
 
 
 </div>
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="<?= BASEURL;?>/mahasiswa/tambah" method="post">
+        	 <div class="form-group">
+			    <label for="nama">Nama</label>
+			    <input type="text" class="form-control" id="nama" name="nama">
+			  </div>
+			  <div class="form-group">
+			    <label for="nrp">Nrp</label>
+			    <input type="text" class="form-control" id="nrp" name="nrp">
+			  </div>
+			  <div class="form-group">
+			    <label for="email">Email</label>
+			    <input type="text" class="form-control" id="email" name="email">
+			  </div>
+
+			  <div class="form-group">
+			    <label for="jurusan">Jurusan</label>
+			    <select class="form-control" id="jurusan" name="jurusan">
+			      <option value="Teknik Informatika">Teknik Informatika</option>
+			      <option value="Teknik Industri">Teknik Industri</option>
+			      <option value="Teknik Mesin">Teknik Mesin</option>
+		
+			    </select>
+			  </div>
+       
+      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+	        <button type="submit" class="btn btn-primary">Tambah Data</button>
+
+	      </div>
+       </form>
+    </div>
+  </div>
+</div>
+
+
